3409e5c72dd253b7f39714c61e3e9c52d39b273c
Michel Belleville <michel.belleville@gmail.com>
2017-04-26 14:54:41 +0200

    show off lodash

--- app/index.js
+++ app/index.js
@@ -1,6 +1,6 @@
-const calc = require('./calc')
+const _ = require('lodash')
 
 const numbersToAdd = [3,4,10,2]
 
-const result = calc.sum(numbersToAdd)
-console.log(`The result is: ${result}`)
+result = _.assign({ 'a': 1 }, { 'b': 2 }, { 'c': 3 });
+console.log('The result is:', result)
