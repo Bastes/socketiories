d4eca28dfc48bf87a401febfa30a0d1190d2c73f
Michel Belleville <michel.belleville@gmail.com>
2017-05-01 11:47:15 +0200

    PORT constant

--- app/index.js
+++ app/index.js
@@ -1,3 +1,6 @@
+const DEFAULT_PORT = 3000
+const PORT = process.env.PORT || DEFAULT_PORT
+
 var app = require('express')()
 var http = require('http').Server(app)
 var io = require('socket.io')(http)
@@ -31,6 +34,6 @@ io.on('connection', function (socket) {
   })
 })
 
-http.listen(3000, function () {
-  console.log('listening on *:3000')
+http.listen(PORT, function () {
+  console.log(`listening on *:${PORT}`)
 })
