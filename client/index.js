6369c097b4fd9a67f55b349e48b922bba0732aeb
Michel Belleville <michel.belleville@gmail.com>
2017-05-03 11:14:13 +0200

    let's go fullscreen

--- client/index.js
+++ client/index.js
@@ -1,5 +1,4 @@
 require('./chat.styl');
 
 var Elm = require('./index.elm');
-var mountNode = document.getElementById('main');
-var app = Elm.Index.embed(mountNode, { websocketUrl: "ws://localhost:3000" });
+var app = Elm.Index.fullscreen({ websocketUrl: "ws://localhost:3000" });
