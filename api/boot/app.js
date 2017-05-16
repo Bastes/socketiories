6438384d7f1889b61763c6c8887576b544c622cf
Michel Belleville <michel.belleville@gmail.com>
2017-05-16 15:43:00 +0200

    passport prep work all in its neat little file

--- api/boot/app.js
+++ api/boot/app.js
@@ -9,35 +9,10 @@ module.exports = function (callback) {
 
   const wss = require('./websocket')(server);
   const DB = require('./database');
-  const session = require('./session')();
-  const sessionParser = session[0];
-  const sessionStore = session[1];
-  const passport = require('./passport')(DB);
-  const cookieParser = require('cookie-parser');
-  const bodyParser = require('body-parser');
-
-  app.use(cookieParser());
-  app.use(bodyParser.urlencoded({ extended: true }));
-  app.use(sessionParser);
-
-  const sessionUser = require('../lib/session-user')(sessionStore, cookieParser, DB);
 
   require('./environments')(process, app, express);
 
-  app.use(passport.initialize());
-  app.use(passport.session());
-
-  app.get('/auth/google', passport.authenticate('google', {
-    session: true,
-    scope: [
-      'https://www.googleapis.com/auth/plus.login',
-      'https://www.googleapis.com/auth/plus.profile.emails.read'
-    ]
-  }));
-
-  app.get('/auth/google/callback',
-      passport.authenticate('google', { failureRedirect: '/login' }),
-      function(req, res) { res.redirect('/'); });
+  const sessionUser = require('./passport')(app, DB);
 
   callback(app, wss, DB, sessionUser);
 
--- api/boot/passport.js
+++ api/boot/passport.js
@@ -1,13 +1,21 @@
-module.exports = function (DB) {
+module.exports = function (app, DB) {
+  const cookieParser = require('cookie-parser');
+  const bodyParser = require('body-parser');
+  const session = require('./session')();
+  const sessionParser = session[0];
+  const sessionStore = session[1];
   const passport = require('passport');
   const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
+
+  const sessionUser = require('../lib/session-user')(sessionStore, cookieParser, DB);
 
   const GoogleStrategyConfig = {
     clientID: process.env.GOOGLE_OAUTH2_CLIENT_ID,
     clientSecret: process.env.GOOGLE_OAUTH2_CLIENT_SECRET,
     callbackURL: `${process.env.SERVER_DOMAIN || ''}/auth/google/callback`
   };
-  const GoogleStrategyCallback = function(accessToken, refreshToken, profile, done) {
+
+  const GoogleStrategyCallback = function (accessToken, refreshToken, profile, done) {
     DB.getInstance(function(db) {
       db.collection("users").update(
           { googleId: profile.id },
@@ -34,5 +42,24 @@ module.exports = function (DB) {
 
   passport.deserializeUser(DB.findUser);
 
-  return passport;
+  app.use(cookieParser());
+  app.use(bodyParser.urlencoded({ extended: true }));
+  app.use(sessionParser);
+
+  app.use(passport.initialize());
+  app.use(passport.session());
+
+  app.get('/auth/google', passport.authenticate('google', {
+    session: true,
+    scope: [
+      'https://www.googleapis.com/auth/plus.login',
+      'https://www.googleapis.com/auth/plus.profile.emails.read'
+    ]
+  }));
+
+  app.get('/auth/google/callback',
+      passport.authenticate('google', { failureRedirect: '/login' }),
+      function(req, res) { res.redirect('/'); });
+
+  return sessionUser;
 };
